b182089054fd0b659a7eb863d39008d4b484c320
Rishijangir566 <rishijangirjr@gmail.com>
2025-01-29 12:53:06 +0530

    new links create

--- Back/index.js
+++ Back/index.js
@@ -7,12 +7,14 @@ import cors from "cors";
 const app = express();
 const port = process.env.PORT
 
+// app.use(cors({ origin: "http://localhost:5173" }));
 app.use(cors({ origin: "https://todolist-fullstack-1-i2zm.onrender.com" }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-await mongoose.connect("mongodb+srv://rishijangirjr:147Egd4xWkAYPPZa@cluster0.dlqaa.mongodb.net/todolistfullstack?retryWrites=true&w=majority&appName=Cluster0");
+// await mongoose.connect("mongodb://127.0.0.1:27017/todolistfullstack"); // use backend and change
+await mongoose.connect("mongodb+srv://rishijangirjr:147Egd4xWkAYPPZa@cluster0.ektln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
 const todoSchema = new mongoose.Schema({
   id: { type: String, required: true },
@@ -36,7 +38,7 @@ app.post("/api/todos/add", async (req, res) => {
     completed
   })
   await newTodo.save();
-  res.status(201).send({ messege: "Todo Saved " });
+  res.status(201).send({ messege: "Todo Saved" });
 
 })
 
@@ -44,7 +46,7 @@ app.put("/api/todos/edit/:id", async (req, res) => {
   const id = req.params.id
   const { title, completed } = req.body;
   await Todo.findOneAndUpdate({ id }, { title, completed });
-  res.send({ messege: "Todo Update" });
+  res.send({ messege: "Todo Updated" });
 })
 
 app.delete("/api/todos/delete/:id", async (req, res) => {
